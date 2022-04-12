18cd98c7d6c76e3dc7fb10fbab25166b1862142d
LucasPBernard <lucas.p.bernard@gmail.com>
2022-04-11 21:08:17 -0500

    4-11-2022

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,21 +1,23 @@
 package org.launchcode.java.studios.areaofacircle;
-
+import java.lang.Double;
 import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
 
 
-        double a;
-        double pi = 3.14;
-        int r;
+        Double a;
+        Double pi = 3.14;
+        Double r;
         Scanner input = new Scanner(System.in);
         System.out.println("What is your radius?");
-        r = input.nextInt();
+        r = input.nextDouble();
         input.close();
-        a = pi * r * r;
-        System.out.println("The area of the circle of radius "+r+"is: "+a);
+//        a = pi * r * r;
+        a = Circle.getArea(r);
+        System.out.println("The radius of the circle "+r);
+
+        System.out.println("The area of the circle of radius "+r+" is: "+a);
 //        String test = `The radius of the circle ${r}`;
-        System.out.printf("The radius of the circle %d",r);
     }
 }
---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,7 @@
+package org.launchcode.java.studios.areaofacircle;
+
+public class Circle {
+    public static Double getArea(Double r){
+        return 3.14 * r * r;
+    }
+}
